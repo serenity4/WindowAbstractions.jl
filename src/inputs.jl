3413f3e1a106880773aece82b45aa6475bd2e822
serenity4 <cedric.bel@hotmail.fr>
2023-02-13 23:56:57 +0100

    Update docstring

--- src/inputs.jl
+++ src/inputs.jl
@@ -20,6 +20,6 @@ MouseButton() = zero(MouseButton)
 
 """
 Construct a KeyCombination instance from a string as `"[<state_1>+[...<state_n>+]]<key>"`.
-The string must be a list of elements separated by '+' characters, with only one non-state character or one fkey as string. For example, `"k"`, `"alt+k"` and `"ctrl+alt+shift+k"` are valid strings, but `"k+a"` is not. Casing is ignored. Fkeys are simply f<1-12> keys, for example `key"alt+f4"` is valid.
+The string must be a list of elements separated by '+' characters, with only one key symbol at the end. For example, `"k"`, `"alt+k"` and `"ctrl+alt+shift+f11"` are valid strings, but `"k+a"` is not. Casing is significant, as `z` and `Z` are different key symbols. For modifier strings, the casing should be lowercase, or will be forcefully converted into a lowercase string (reducing performance).
 """
 macro key_str(expr) esc(:(KeyCombination($(Meta.parse("\"$(escape_string(expr))\""))))) end
